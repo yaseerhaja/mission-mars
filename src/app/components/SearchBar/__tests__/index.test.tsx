7b3df75ec9b1e770da0f8f74c2991adf10ea5c8a
haja <mohaideen.kudhbudeen@klm.com>
2023-03-10 09:55:06 +0100

    test spec

--- src/app/components/SearchBar/__tests__/index.test.tsx
+++ src/app/components/SearchBar/__tests__/index.test.tsx
@@ -1,10 +1,21 @@
-import { render } from "@testing-library/react";
+import TextField from "@mui/material/TextField";
 
+import { shallow, render } from "enzyme";
 import { SearchBar } from "..";
 
 describe("<SearchBar  />", () => {
   it("should match snapshot", () => {
-    const loadingIndicator = render(<SearchBar />);
+    const loadingIndicator = render(<SearchBar filterBy={undefined} />);
     expect(loadingIndicator.container.firstChild).toMatchSnapshot();
+  });
+  it("should call onChange prop", () => {
+    const onSearchMock = jest.fn();
+    const event = {
+      preventDefault() {},
+      target: { value: "the-value" },
+    };
+    const component = shallow(<TextField onChange={onSearchMock} />);
+    component.find("input").simulate("change", event);
+    expect(onSearchMock).toBeCalledWith("the-value");
   });
 });
